978fdc00893e0acfc3010b108d3f3068b431fc44
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-10-25 16:41:06 +0700

    fin.

---
+++ src/main/java/lanaDrahrepus/controllers/CustomerController.java
@@ -0,0 +1,20 @@
+package lanaDrahrepus.controllers;
+
+import lanaDrahrepus.service.CustomerService;
+import lanaDrahrepus.service.CustomerServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class CustomerController {
+    private CustomerService customerService = new CustomerServiceImpl();
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("customers", customerService.findAll());
+        return "index";
+    }
+}
